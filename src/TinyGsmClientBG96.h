a06f4dd4a5614030de6e9105f012d3fe7d8ecffe
moto <j3tm0t0@gmail.com>
2020-12-02 11:59:37 +0900

    change to use AT+CEREG to check registration status

--- src/TinyGsmClientBG96.h
+++ src/TinyGsmClientBG96.h
@@ -235,7 +235,7 @@ class TinyGsmBG96 : public TinyGsmModem<TinyGsmBG96>,
    */
  public:
   RegStatus getRegistrationStatus() {
-    return (RegStatus)getRegistrationStatusXREG("CREG");
+    return (RegStatus)getRegistrationStatusXREG("CEREG");
   }
 
  protected:
